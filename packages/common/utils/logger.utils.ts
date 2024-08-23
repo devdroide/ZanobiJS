19d12297b04d5ddae9daa17ecc81d0e608c6f0e7
devdroide <johne.aplicativos@gmail.com>
2024-08-23 09:45:33 -0500

    :sparkles: ZNB008 adjusted service to print logs

--- packages/common/utils/logger.utils.ts
+++ packages/common/utils/logger.utils.ts
@@ -1,4 +1,4 @@
-import { ILoggerService } from "../interfaces";
+import { ILoggerService, IOptionsLog } from "../interfaces";
 import { LoggerService } from "../services/logger.service";
 import { LoggerUserService } from "../services/logger.user.service";
 
@@ -9,10 +9,13 @@ import { LoggerUserService } from "../services/logger.user.service";
  * ```typescript
  * const logger = Logger();
  * logger.info('Este es un mensaje informativo');
+ * const logger = Logger({ withColor: false });
+ * logger.info('Este es un mensaje informativo'); // print without color in console
  * ```
  *
  * @returns La única instancia de `LoggerService` que implementa la interfaz `ILoggerService`.
  */
-export const Logger = (): ILoggerService => LoggerService.getInstance();
-export const LoggerUser = (): ILoggerService => LoggerUserService.getInstance();
-
+export const Logger = (options?: IOptionsLog): ILoggerService =>
+  LoggerService.getInstance(options);
+export const LoggerUser = (options?: IOptionsLog): ILoggerService =>
+  LoggerUserService.getInstance(options);
